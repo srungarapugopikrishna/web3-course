dfff8368e1f743bd19eb3c5d7f81952d777db832
Gopi Krishna <srungarapugopikrishna@gmail.com>
2022-10-28 18:57:25 +0530

    modifying day1/provider

--- day1/providers.js
+++ day1/providers.js
@@ -1,21 +1,25 @@
-import * as dotenv from 'dotenv' 
+import * as dotenv from "dotenv";
 dotenv.config();
 import { ethers } from "ethers";
 
-const infuraUrl = 'https://mainnet.infura.io/v3/dbe239153b1e4f1cae2eeff42405d071';
+const infuraUrl =
+  "https://mainnet.infura.io/v3/dbe239153b1e4f1cae2eeff42405d071";
 const provider = new ethers.providers.JsonRpcProvider(infuraUrl);
 
 // console.log('provider----->>>>>');
 // console.log(provider);
 // console.log('<<<<<------provider');
-// console.log("Current block number", await provider.getBlockNumber());
+console.log("Current block number", await provider.getBlockNumber());
 
-// console.log("ens to address", await provider.resolveName("atg.eth"));
+console.log("ens to address", await provider.resolveName("atg.eth"));
 
-// console.log("address to ens", await provider.lookupAddress("0x34aA3F359A9D614239015126635CE7732c18fDF3"));
+console.log(
+  "address to ens",
+  await provider.lookupAddress("0x34aA3F359A9D614239015126635CE7732c18fDF3")
+);
 
 const vitalikBalance = await provider.getBalance("vitalik.eth");
-// console.log("vitalik.eth has", ethers.utils.formatEther(vitalikBalance));
+
 let sanfordBalance = await provider.getBalance("sanfordstout.eth");
 
 sanfordBalance = sanfordBalance.add(ethers.utils.parseEther("5000"));
@@ -24,4 +28,7 @@ console.log("vitalik balance", ethers.utils.formatEther(vitalikBalance));
 
 console.log("sanford balance", ethers.utils.formatEther(sanfordBalance));
 
-// console.log("1.5 eth is ", ethers.utils.formatEther(ethers.utils.parseEther("1.5")));
+// console.log(
+//   "1.5 eth is ",
+//   ethers.utils.formatEther(ethers.utils.parseEther("1.5"))
+// );
