fa85bf9c5b829eb98499d542f13b32d8c4d98f0a
Gopi Krishna <srungarapugopikrishna@gmail.com>
2022-10-29 19:22:43 +0530

    adding

--- day3/hardhat.config.js
+++ day3/hardhat.config.js
@@ -2,6 +2,6 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "localhost",
+  // defaultNetwork: "localhost",
   solidity: "0.8.17",
 };
