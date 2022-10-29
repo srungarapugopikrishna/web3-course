acf5f60a5e8bedd463afec70e73515e1b1350fc6
Gopi Krishna <srungarapugopikrishna@gmail.com>
2022-10-29 19:04:44 +0530

    Adding greetContractInteract.js

--- day3/scripts/greetContractInteract.js
+++ day3/scripts/greetContractInteract.js
@@ -1,26 +1,21 @@
 const hre = require("hardhat");
 const { ethers, BigNumber } = require("ethers");
-// const { getProvider, getSigner } = require("../../day2/utils");
 
 async function main() {
-  const hardhatSigner = (await hre.ethers.getSigners())[0];
+  const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+  const greeter = await hre.ethers.getContractAt("Greeter", contractAddress);
+  console.log("Initial greeting:::", await greeter.greet());
 
-  const myBalance = await hardhatSigner.getBalance();
+  console.log("sending greeting....");
+  const setTx = await greeter.setGreeting("Modifying to new Greet message");
 
-  const toAddress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+  console.log("setTx sent!");
 
-  console.log("Sending ETH to: ", toAddress);
+  await setTx.wait();
 
-  const tx = await hardhatSigner.sendTransaction({
-    to: toAddress,
-    value: myBalance.div(BigNumber.from(10)),
-  });
+  console.log("setTx mined!");
 
-  console.log("TX SENT!", tx.hash);
-
-  await tx.wait();
-
-  console.log("TX MINED!");
+  console.log("Updated greeting:::", await greeter.greet());
 }
 
 main().catch((error) => {
