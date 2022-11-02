f3210adecce4e866cb5a6f649f065cc85d2af500
Gopi Krishna <srungarapugopikrishna@gmail.com>
2022-11-02 21:43:14 +0530

    adding new code

--- day4/contracts/KrishnaToken.sol
+++ day4/contracts/KrishnaToken.sol
@@ -3,11 +3,24 @@ pragma solidity ^0.8.13;
 
 contract KrishToken{
     uint256 public constant totalSupply = 1000;
-    uint256 public constant totalCreated = 0;
+    uint256 public totalCreated = 0;
+
+    uint256 public constant CREATION_PRICE = 0.01 ether;
+
+    address public immutable boss;
 
     mapping(address => uint256) public balances;
 
-    function create(uint256 quantity) public {
+    constructor(){
+        boss = msg.sender;
+    }
+
+    modifier onlyBoss(){
+        require(msg.sender == boss, "Sorry, not the boss");
+        _;
+    }
+
+    function create(uint256 quantity) public onlyBoss {
         require(quantity + totalCreated <= totalSupply, "totalSupply reached!");
 
         balances[msg.sender] += quantity;
@@ -15,9 +28,18 @@ contract KrishToken{
     }
 
     function send(address to, uint256 quantity) public {
-        require(balances[msg.sender] >= quantity, "You don't have enough);
+        require(balances[msg.sender] >= quantity, "You don't have enough");
 
         balances[msg.sender] -= quantity;
         balances[to] += quantity;
     }
+
+    function buy() public payable {
+        require(msg.value >= CREATION_PRICE);
+        require(totalCreated < totalSupply, "totalSupply reached!");
+
+        balances[msg.sender] += 1;
+        totalCreated += 1;
+    }
+
 }
--- day4/test/token-test.js
+++ day4/test/token-test.js
@@ -5,15 +5,49 @@ describe("Token", function () {
   let token;
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("KrishToken");
-    const token = await Token.deploy();
+    token = await Token.deploy();
     await token.deployed();
   });
+
   it("Should be able to create Tokens", async function () {
     const [signer0, signer1] = await ethers.getSigners();
     const createTx = await token.create(100);
     await createTx.wait();
 
     expect(await token.balances(signer0.address)).to.equal(100);
-    // expect(await token.balances(signer1.address)).to.equal(0);
+    expect(await token.balances(signer1.address)).to.equal(0);
+  });
+
+  it("Should revert if creating more than total supply", async function () {
+    const totalSupply = await token.totalSupply();
+    const createTx = await token.create(totalSupply.add(100));
+
+    // expect(createTx.wait()).to.be.reverted;
+    await expect(token.create(totalSupply.add(100))).to.be.revertedWith(
+      "Some revrt message"
+    );
+  });
+
+  it("Should be able to send Tokens", async function () {
+    const [signer0, signer1] = await ethers.getSigners();
+    const createTx = await token.create(100);
+    await createTx.wait();
+
+    const sendTx = await token.send(signer1.address, 5);
+    await sendTx.wait();
+
+    expect(await token.balances(signer0.address)).to.equal(95);
+    expect(await token.balances(signer1.address)).to.equal(5);
+  });
+
+  it("Should allow random user to buy tokens", async function () {
+    const [signer0, signer1] = await ethers.getSigners();
+
+    const buyTx = await token.connect(signer1).buy({
+      value: ethers.utils.parseEther("0.01"),
+    });
+    await buyTx.wait();
+
+    expect(await token.balances(signer1.address)).to.equal(1);
   });
 });
